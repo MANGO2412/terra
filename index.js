109bb072878c55c88c41cb2d332fd77846d76733
MANGO2412 <manuelgom3z22052003@gmail.com>
2023-08-09 00:22:17 -0700

    config

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ import "./src/database.js";
 app.listen(app.get("port"))
 console.log(`Server is running on port ${app.get("port")}`);
 
+
+
+app.keepAliveTimeout = 120 * 1000;
+app.headersTimeout = 120 * 1000;
+
+
+
